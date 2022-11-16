6d82a53abaa4a1c9f1d4e24f74cfd31782675f34
kaw393939 <keith@webizly.com>
2022-11-16 15:02:45 -0500

    reset

--- config.py
+++
@@ -1,8 +0,0 @@
-"""QR Code Generator Configuration"""
-import os
-
-
-class Config:
-    QR_CODE_IMAGE_DIRECTORY = os.environ.get('QR_CODE_IMAGE_DIRECTORY')
-    QR_CODE_DEFAULT_URL = os.environ.get('QR_CODE_DEFAULT_URL')
-    QR_CODE_DEFAULT_FILE_NAME = os.environ.get('QR_CODE_DEFAULT_FILE_NAME')
